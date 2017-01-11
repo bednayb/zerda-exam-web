e4d330c4356278e2c28d1774d9e742579b519402
bednayb <bednaybence@gmail.com>
2017-01-11 11:45:59 +0100

    change frontend

--- client/client.js
+++ client/client.js
@@ -1,21 +1,28 @@
 'use strict';
 
 var button = document.querySelector('#btn');
-var feedback = document.querySelector('#feedback');
+var feedback = document.querySelector('#inputText');
 var scale = document.querySelector('#scale');
-var email = document.querySelector('#email');
+var emailUser = document.querySelector('#userEmail');
 
 
 button.addEventListener('click',function () {
    // CHECK IT WORKS OR NOT
-   console.log("button works");
+   // console.log("button works");
+   // console.log(scale.value);
+   // console.log(feedback.value);
+   // console.log(emailUser.value)
    // CHECK IT WORKS OR NOT
 
    var httpRequest = new XMLHttpRequest();
  httpRequest.open('POST', 'http://localhost:3000/exam', true);
  httpRequest.setRequestHeader('Content-Type', 'application/json');
  httpRequest.send(JSON.stringify({
-   feedback: feedback.value,
-   scale: scale.value,
-   email:email.value
+
+   //  input'value
+   feedbackValue: feedback.value,
+   scaleValue: scale.value,
+   emailValue: emailUser.value
+}
+))
 });
