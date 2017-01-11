490a00d65f0266b4ccbd8a1962bc850d30e0b4a1
bednayb <bednaybence@gmail.com>
2017-01-11 11:44:55 +0100

    change small stuff on server.js

--- server.js
+++ server.js
@@ -4,13 +4,27 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var mysql = require('mysql');
-// var decrypt = require('./decrypt.js');
+var validator = require('./validator.js');
 
 
 var app = express();
 app.use(bodyParser.json());
 app.use(express.static('client'));
 
+app.post('/exam', function(req, res) {
+  // console.log(req.body); // { shift: 3, text: 'oruhp lsvxp groru vlw' }
+  var feedback = req.body.feedbackValue;
+  var scale = req.body.scaleValue;
+  var email = req.body.email.Value
+  // var decrypted = decrypt(feedback,email,decrypted);
 
+  var response = {
+    "feedback" : req.body.feedbackValue,
+    "scale" : req.body.scaleValue,
+    "email" : req.body.email.Value
+  }
+   res.send(response);
+}
+);
 
 app.listen(3000);
