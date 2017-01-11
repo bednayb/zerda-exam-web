d1672abe4078948f3055b28439664f2542809084
bednayb <bednaybence@gmail.com>
2017-01-11 10:10:49 +0100

    change ID-s and make httpRequest to the button

--- client/client.js
+++ client/client.js
@@ -1,7 +1,21 @@
 'use strict';
 
 var button = document.querySelector('#btn');
+var feedback = document.querySelector('#feedback');
+var scale = document.querySelector('#scale');
+var email = document.querySelector('#email');
+
 
 button.addEventListener('click',function () {
+   // CHECK IT WORKS OR NOT
    console.log("button works");
+   // CHECK IT WORKS OR NOT
+
+   var httpRequest = new XMLHttpRequest();
+ httpRequest.open('POST', 'http://localhost:3000/exam', true);
+ httpRequest.setRequestHeader('Content-Type', 'application/json');
+ httpRequest.send(JSON.stringify({
+   feedback: feedback.value,
+   scale: scale.value,
+   email:email.value
 });
