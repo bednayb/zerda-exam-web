0c4d39ad2d4460fbb55d9ec6cde7e9ffcdf0dfcd
bednayb <bednaybence@gmail.com>
2017-01-11 11:44:18 +0100

    add validator file

---
+++ validator.js
@@ -0,0 +1,19 @@
+'use strict';
+
+
+// THE LIST
+var list = ["amazing", "awesome", "blithesome", "excellent", "fabulous", "fantastic", "favorable", "fortuitous", "great", "incredible", "ineffable", "mirthful", "outstanding", "perfect", "propitious", "remarkable", "smart", "spectacular", "splendid", "stellar", "stupendous", "super", "ultimate", "unbelievable", "wondrous"];
+
+
+// THE FUNCTION WHAT WILL BE TESTED at test.js
+function decrypt(email,scale) {
+   if(
+      email.indexOf("@") != -1 && scale > 10
+   ) {
+   return "validator accepts valid email, and good scale"
+}
+
+};
+
+
+module.exports = decrypt;
