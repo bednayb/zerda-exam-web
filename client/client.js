01b2a86d925cde2de01535666992ba9f7fad8503
bednayb <bednaybence@gmail.com>
2017-01-11 12:48:54 +0100

    small changes

--- client/client.js
+++ client/client.js
@@ -4,29 +4,22 @@ var button = document.querySelector('#btn');
 var feedback = document.querySelector('#inputText');
 var scale = document.querySelector('#scale');
 var emailUser = document.querySelector('#userEmail');
-
 var loading = document.querySelector('span');
 
 button.addEventListener('click',function () {
-   // CHECK IT WORKS OR NOT
-   // console.log("button works");
-   // console.log(scale.value);
-   // console.log(feedback.value);
-   // console.log(emailUser.value)
-   // CHECK IT WORKS OR NOT
 
-// in the task it was not necesseary hide anytime
-loading.innerHTML = "loading";
+
+   // in the task it was not necesseary hide anytime
+   loading.innerHTML = "loading";
 
    var httpRequest = new XMLHttpRequest();
- httpRequest.open('POST', 'http://localhost:3000/exam', true);
- httpRequest.setRequestHeader('Content-Type', 'application/json');
- httpRequest.send(JSON.stringify({
-
-   //  input'value
-   feedbackValue: feedback.value,
-   scaleValue: scale.value,
-   emailValue: emailUser.value
-}
-))
+   httpRequest.open('POST', 'http://localhost:3000/exam', true);
+   httpRequest.setRequestHeader('Content-Type', 'application/json');
+   httpRequest.send(JSON.stringify({
+      //  input'value
+      feedback: feedback.value,
+      scale: scale.value,
+      email: emailUser.value
+         }
+   ))
 });
--- server.js
+++ server.js
@@ -16,18 +16,24 @@ app.post('/exam', function(req, res) {
   var feedback = req.body.feedbackValue;
   var scale = req.body.scaleValue;
   var email = req.body.emailValue;
-  var decrypted = validator(email,decrypted);
-  console.log(decrypted);
 
-  var response = {
-     "status" : "ok",
-    "feedback" : req.body.feedbackValue,
-    "scale" : req.body.scaleValue,
-    "email" : req.body.emailValue
-  }
-  console.log(response);
-   res.send(response);
-}
+  if (validator(email,scale) != true) {
+     console.log('alma');
+    res.status(400);
+    var response = {
+    "status": "error",
+    "message": "thank you"
+    };
+    res.send(response);
+    }else{
+       var response = {
+          "feedback" : feedback,
+          "scale" : scale,
+          "email" : email
+         }
+         res.send(response);
+      }
+   }
 );
 
 app.listen(3000);
