1041d7a38d723f940f74bb290b9df3585b464f14
bednayb <bednaybence@gmail.com>
2017-01-11 11:43:47 +0100

    add test file

--- test.js
+++ test.js
@@ -1,7 +1,20 @@
 'use strict';
 
 var test = require('tape');
+var validator = require('./validator.js');
 
 test("validator accepts valid email, a scale of 15 and positive feedback");
 test("validator rejects unfilled email, a scale of 15 and positive feedback");
 test("validator rejects valid email, a scale of 9 and positive feedback");
+
+
+//test the tape
+test('true', function (t) {
+  t.equal(1,1);
+  t.end();
+})
+
+test('email', function (t) {
+  t.equal(validator("toto@gmail.com",18),'validator accepts valid email, and good scale');
+  t.end();
+})
